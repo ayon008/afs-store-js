ef7ef9b6cd53f90f6a2525904378753a3f67248b
ayon008 <shariar.ayon128@gmail.com>
2025-12-19 17:29:16 +0600

    first commit

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-4 z-50 bg-red-400 sticky left-0 right-0 top-0'>
+        <p>Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/fonts/Alliance.js
@@ -0,0 +1,10 @@
+import localFont from 'next/font/local'
+
+export const alliance = localFont({
+    src: [
+        { path: '../../public/AllianceNo2-Bold.woff2', weight: '700', style: 'normal' },
+        { path: '../../public/AllianceNo2-Regular (1).woff2', weight: '400', style: 'normal' },
+        { path: '../../public/AllianceNo2-Light-1.woff2', weight: '500', style: 'normal' },
+        { path: '../../public/AllianceNo2-SemiBold.woff2', weight: '600', style: 'normal' }
+    ]
+})
